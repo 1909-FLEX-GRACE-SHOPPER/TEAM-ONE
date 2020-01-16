5ed5cd4bf5d98703eb3518ac5448af15e5c77bf7
Ries Nicolas <NRies@schools.nyc.gov>
2020-01-16 10:54:08 -0500

    added login and sign up pages

---
+++ app/components/Signup.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Form from "react-bootstrap";
+import Button from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  return (
+    <div className="signup">
+      <div className="logo-medium"></div>
+      <Form className="signup-form">
+        <Form.Group>
+          <Form.Label>FIRST NAME</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>LAST NAME</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>USER NAME</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>PASSWORD</Form.Label>
+          <Form.Control type="password" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>CONFIRM PASSWORD</Form.Label>
+          <Form.Control type="password" />
+        </Form.Group>
+        <div>
+          Already a user? <Link>Login</Link>
+        </div>
+        <Button variant="primary" type="submit">
+          SIGN UP
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Signup;
