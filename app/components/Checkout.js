7ea195d1cfe09efafb6cc6db06633111b81125a4
Ries Nicolas <NRies@schools.nyc.gov>
2020-01-16 10:39:25 -0500

    added checkout page

---
+++ app/components/Checkout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import CreditCard from "./CreditCard.js";
+import Shipping from "./Shipping.js";
+import Button from "react-bootstrap";
+
+const Checkout = ({ orderDetails }) => {
+  return (
+    <div className="checkout-page">
+      <Link>Back</Link>
+      <div>CHECKOUT - {orderDetails.userType}</div>
+      <div>BILLING</div>
+      <CreditCard />
+      <Shipping />
+      <Button>PROCEED TO CONFIRMATION</Button>
+    </div>
+  );
+};
+
+export default Checkout;
---
+++ app/components/CreditCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreditCard = ({ cardDetails }) => {
+  <div>This is the credit card view</div>;
+};
+
+export default CreditCard;
