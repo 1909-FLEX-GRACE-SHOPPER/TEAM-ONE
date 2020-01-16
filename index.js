6d5c8f539145050ad46ab19cdc8181e3522973b1
Ries Nicolas <NRies@schools.nyc.gov>
2020-01-16 10:02:56 -0500

    added wishlist and wishlist item components

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-app.use(express.static(path.join('__dirname', '..', '/public')));
+const path = require("path");
+app.use(express.static(path.join("__dirname", "..", "/public")));
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-// Send index.html for any other requests
 app.listen(PORT, () => {
-  console.log(`App is listening at localhost:${3000}`);
+  console.log(`App is listening at localhost:${PORT}`);
 });
