2aad3c71069166ef1ad1d38c0f9eb5b0457e9d70
Cissy Bai <53950748+wulalasuperdope@users.noreply.github.com>
2020-01-19 16:30:09 -0500

    add .env file to make Heroku connect to database

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,7 @@
 // This module is to establish a connection to the Postgres database
 // by creating a Sequelize instance (called "db").
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/grace-shopper', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/grace-shopper', {
     logging: false,
 });
 
