9f7e799f568b15d3d274cd3c59054ccfcb2baf59
Cissy Bai <53950748+wulalasuperdope@users.noreply.github.com>
2020-01-19 18:35:10 -0500

    allow ship & bill address and payment to be null

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -55,31 +55,9 @@ const Users = db.define('users', {
     },
     firstName: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'First name cannot be empty',
-            },
-            notNull: {
-                arg: true,
-                msg: 'First name cannot be null',
-            },
-        },
     },
     lastName: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Last name cannot be empty',
-            },
-            notNull: {
-                arg: true,
-                msg: 'Last name cannot be null',
-            },
-        },
     },
     phone: {
         type: INTEGER,
@@ -96,45 +74,12 @@ const Users = db.define('users', {
     },
     shippingAddress: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Shipping address cannot be empty',
-            },
-            notNull: {
-                args: true,
-                msg: 'Shipping address cannot be null',
-            },
-        }
     },
     shippingCity: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Shipping city cannot be empty',
-            },
-            notNull: {
-                args: true,
-                msg: 'Shipping city cannot be null',
-            },
-        },
     },
     shippingState: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Shipping state cannot be empty',
-            },
-            notNull: {
-                args: true,
-                msg: 'Shipping state cannot be null',
-            },
-        },
     },
     shippingZip: {
         type: INTEGER,
@@ -162,31 +107,9 @@ const Users = db.define('users', {
     },
     cardholder: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Cardholder cannot be empty',
-            },
-            notNull: {
-                arg: true,
-                msg: 'Cardholder cannot be null',
-            },
-        },
     },
     expirationDate: {
         type: DATEONLY,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Expiration date cannot be empty',
-            },
-            notNull: {
-                arg: true,
-                msg: 'Expiration date cannot be null',
-            },
-        },
     },
     securityCode: {
         type: INTEGER,
@@ -202,45 +125,12 @@ const Users = db.define('users', {
     },
     billingAddress: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Billing address cannot be empty',
-            },
-            notNull: {
-                args: true,
-                msg: 'Billing address cannot be null',
-            },
-        }
     },
     billingCity: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Billing city cannot be empty',
-            },
-            notNull: {
-                args: true,
-                msg: 'Billing city cannot be null',
-            },
-        },
     },
     billingState: {
         type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                args: true,
-                msg: 'Billing state cannot be empty',
-            },
-            notNull: {
-                args: true,
-                msg: 'Billing state cannot be null',
-            },
-        },
     },
     billingZip: {
         type: INTEGER,
