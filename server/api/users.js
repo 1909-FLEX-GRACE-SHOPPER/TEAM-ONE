fd0f067a19cc4cd353a9ac6b84bad8d56c846643
Raymond Ng <rng@rng-m01.local>
2020-01-27 17:29:07 -0500

    update for only one  cookie

--- server/api/users.js
+++ server/api/users.js
@@ -80,11 +80,7 @@ router.post('/login', (req, res, next) => {
           {
             where: { email, password },
             returning: true
-          },
-          res.cookie('uuid', userOrNull.id, {
-            path: '/',
-            expires: new Date(Date.now() + 1000 * 60 * 60 * 24)
-          })
+          }
         );
         return res.status(202).send(userOrNull);
       }
