b09a26d3bb0a3ebe8854714cb0af46fbdc2ff24f
Raymond Ng <raymond.ng47@gmail.com>
2020-02-01 10:54:45 -0500

    update linting on the nav component

--- app/components/Login.js
+++ app/components/Login.js
@@ -73,7 +73,6 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     logIn: credentials => dispatch(logInUser(credentials))
-
   };
 };
 
--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -16,7 +16,7 @@ class Navigation extends Component {
 			case 'Admin':
 				return (
 					<Nav>
-						<Nav.Link href='/products/add'> Add a Product </Nav.Link>
+						<Nav.Link href="/products/add"> Add a Product </Nav.Link>
 						<Nav.Link href={`/user/${params.id}`}> {params.firstName} {params.lastName} </Nav.Link>
 						<Button> Logout </Button>
 					</Nav>
@@ -24,8 +24,8 @@ class Navigation extends Component {
 			default:
 				return (
 					<Nav>
-						<Nav.Link to='/signup' href='/signup'> Sign Up </Nav.Link>
-						<Nav.Link to='/login' href='/login'> Login </Nav.Link>
+						<Nav.Link href="/signup"> Sign Up </Nav.Link>
+						<Nav.Link href="/login"> Login </Nav.Link>
 					</Nav>
 				);
 		}
@@ -33,12 +33,12 @@ class Navigation extends Component {
 	render() {
 		const { user } = this.props;
 		return (
-			<Navbar bg='dark' variant='dark'>
+			<Navbar bg="dark" variant="dark">
 				<Navbar.Brand>Logo</Navbar.Brand>
-				<Nav className='mr-auto'>
-					<Nav.Link href='/home'>Home</Nav.Link>
-					<Nav.Link href='/about'>About</Nav.Link>
-					<Nav.Link href='/products'>Shop</Nav.Link>
+				<Nav className="mr-auto">
+					<Nav.Link href="/home">Home</Nav.Link>
+					<Nav.Link href="/about">About</Nav.Link>
+					<Nav.Link href="/products">Shop</Nav.Link>
 				</Nav>
 				<Nav>
 					<Nav.Link href={`/shoppingcart/${user.id}`}>Cart</Nav.Link>
