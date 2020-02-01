b6f7d856bc0aa1c74942b79bb1861ef4d9e2f0f9
Shel <orockshel@gmail.com>
2020-02-01 12:11:31 -0500

    Update seed.js
    //In this function, store all the users into variables. you can do this easily with array destructuring.
    //(i.e. const [user1, user2, user3 ... ] = Promise.all(users.map ...)
    //After the users are done seeding and stored in variabled, when you Create the cart items. I think I would keep the amount
    //of created carts small (maybe 3/4 max for proof of concept), and do this one-by-one without Promise.all.
    //You can now set the foreign keys to ids stored in variables (userId = user1.id). The problem before is that
    //the userId was changing everytime it seeded, so you couldn't hardcode in an id. LMK any questions.

--- seed.js
+++ seed.js
@@ -3,6 +3,13 @@ const { User, Product, Cart } = require('./server/db/models/index.js');
 const { db } = require('./server/db/index.js');
 const { green, red } = require('chalk');
 //TODO: seed userId in Cart database
+
+//In this function, store all the users into variables. you can do this easily with array destructuring.
+//(i.e. const [user1, user2, user3 ... ] = Promise.all(users.map ...)
+//After the users are done seeding and stored in variabled, when you Create the cart items. I think I would keep the amount
+//of created carts small (maybe 3/4 max for proof of concept), and do this one-by-one without Promise.all.
+//You can now set the foreign keys to ids stored in variables (userId = user1.id). The problem before is that
+//the userId was changing everytime it seeded, so you couldn't hardcode in an id. LMK any questions.
 const seedUsers = async () => {
   await Promise.all(users.map(_u => User.create(_u))).then(() =>
     Promise.all(cart.map(product => Cart.create(product)))
