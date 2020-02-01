d49fed9c0a038b89cdb28c4607c36ecd8f082f8f
Raymond Ng <raymond.ng47@gmail.com>
2020-01-31 22:14:49 -0500

    updated with switch cases

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -2,25 +2,48 @@ import React, { Component } from 'react';
 import { Nav, Navbar, Button } from 'react-bootstrap';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import NavGuest from './NavGuest';
-import NavUser from './NavUser';
 
 class Navigation extends Component {
+	switchNavBar = (params) => {
+		switch (params.userType) {
+			case 'Existing customer':
+				return (
+					<Nav>
+						<Nav.Link href={`/user/${params.id}`}> {params.firstName} {params.lastName} </Nav.Link>
+						<Button> Logout </Button>
+					</Nav>
+				);
+			case 'Admin':
+				return (
+					<Nav>
+						<Nav.Link href="/products/add"> Add a Product </Nav.Link>
+						<Nav.Link href={`/user/${params.id}`}> {params.firstName} {params.lastName} </Nav.Link>
+						<Button> Logout </Button>
+					</Nav>
+				);
+			default:
+				return (
+					<Nav>
+						<Nav.Link to="/signup" href="/signup"> Sign Up </Nav.Link>
+						<Nav.Link to="/login" href="/login"> Login </Nav.Link>
+					</Nav>
+				);
+		}
+	};
 	render() {
 		const { user } = this.props;
-		const links = user.firstName ? <NavUser/> : <NavGuest />;
 		return (
 			<Navbar bg='dark' variant='dark'>
 				<Navbar.Brand>Logo</Navbar.Brand>
 				<Nav className='mr-auto'>
-					<Nav.Link href='/home'>Home</Nav.Link>
-					<Nav.Link href='/about'>About</Nav.Link>
-					<Nav.Link href='/products'>Shop</Nav.Link>
+					<Nav.Link href="/home">Home</Nav.Link>
+					<Nav.Link href="/about">About</Nav.Link>
+					<Nav.Link href="/products">Shop</Nav.Link>
 				</Nav>
 				<Nav>
-					<Nav.Link href='/shoppingcart/:userId'>Cart</Nav.Link>
+					<Nav.Link href={`/shoppingcart/${user.id}`}>Cart</Nav.Link>
 					{/* this is just a temporary link that goes no-where for now, will update once the cart component is ready */}
-					{links}
+					{this.switchNavBar(user)}
 				</Nav>
 			</Navbar>
 		);
