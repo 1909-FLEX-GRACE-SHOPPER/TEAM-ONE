3743cf9b81d75499ad2e127bcefa4db84f8131aa
Nicolas Ries <nries1@gmail.com>
2020-02-02 21:53:14 -0500

    added filter functionality to producs component

--- app/components/Products.js
+++ app/components/Products.js
@@ -6,26 +6,40 @@ import { fetchProducts } from '../redux/thunks/ProductThunks.js';
 import Form from 'react-bootstrap/Form';
 
 class Products extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tag: ''
+    };
+  }
   componentDidMount() {
     this.props.fetchProducts();
   }
+  filterProducts(tag) {
+    this.setState({ tag });
+  }
   render() {
     let { products } = this.props;
+    const { tag } = this.state;
     products = products.rows || [];
     const PRODUCTS_PER_PAGE = 10;
     return (
       <div>
         <div className="product-search-and-pagination">
           <div className="filter-and-sort">
-            <Form.Control as="select" id="filter-by">
-              <option value="">Filter By</option>
-              <option value="option 1">Option 1</option>
-              <option value="option 2">Option 2</option>
-            </Form.Control>
-            <Form.Control as="select" id="sort-by">
-              <option value="">Sort By</option>
-              <option value="option 1">Option 1</option>
-              <option value="option 2">Option 2</option>
+            <Form.Label>Filters</Form.Label>
+            <Form.Control
+              as="select"
+              id="filter-by"
+              onChange={e => {
+                this.filterProducts(e.target.value);
+              }}
+            >
+              <option value="">None</option>
+              <option value="Accessory">Accessory</option>
+              <option value="Charger">Charger</option>
+              <option value="Device">Device</option>
+              <option value="Pod">Pod</option>
             </Form.Control>
           </div>
           <PageSelect pages={products.length / PRODUCTS_PER_PAGE} />
@@ -33,9 +47,13 @@ class Products extends React.Component {
         <div className="all-products-container">
           {products.length === 0
             ? 'No products'
-            : products.map(_product => (
-                <Product key={`product-${_product.id}`} product={_product} />
-              ))}
+            : products.map(_product =>
+                tag === '' || _product.tags === tag ? (
+                  <Product key={`product-${_product.id}`} product={_product} />
+                ) : (
+                  ''
+                )
+              )}
         </div>
         <PageSelect pages={products.length / PRODUCTS_PER_PAGE} />
       </div>
