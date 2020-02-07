1fb115e8a747db4b2cad3509f63096765d0c2c2a
Nicolas Ries <nries1@gmail.com>
2020-02-06 20:00:55 -0500

    merge-users

--- server/api/utils.js
+++ server/api/utils.js
@@ -1,3 +1,6 @@
+const { models } = require('../db/index.js');
+const { User, Order, Cart } = models;
+
 //Pagination middleware.
 //TODO: Return data when the limit is less than the number of rows on a "page" of the table
 const paginate = model => {
@@ -47,15 +50,14 @@ function CartObject(cart) {
   this.cardHolder = cart.cardHolder;
   this.cardNumber = cart.cardNumber;
   this.securityCode = cart.securityCode;
-  if(cart.expirationDate) {
-    this.expirationDate = `${ cart.expirationDate.month } / ${ cart.expirationDate.year }`;
+  if (cart.expirationDate) {
+    this.expirationDate = `${cart.expirationDate.month} / ${cart.expirationDate.year}`;
   }
 }
 
-
 function OrderObject(id, order) {
   this.userId = id;
-  this.orderCost = 100.00;
+  this.orderCost = 100.0;
   this.shippingName = order.shippingName;
   this.shippingAddress = order.shippingAddress;
   this.shippingCity = order.shippingCity;
@@ -69,4 +71,63 @@ function OrderObject(id, order) {
   this.expirationDate = order.expirationDate;
 }
 
-module.exports = { paginate, UserObject, CartObject, OrderObject };
+// Update the orders table and cart table so that
+// any orders or cart items that used to belong to
+// the guest will belong to the new user
+const mergeAndDestroyUser = async (newUser, guestUserInfo) => {
+  const guestUser = await User.findOne({
+    where: { ...guestUserInfo }
+  });
+  console.log('FOUND GUEST USER');
+  try {
+    await User.destroy({
+      where: {
+        ...guestUserInfo
+      }
+    });
+    console.log('DESTROYED USER');
+  } catch (e) {
+    console.log('FAILED TO DESTROY GUEST USER');
+    console.error(e);
+    return new Error(e);
+  }
+  try {
+    await Order.update(
+      { userId: newUser.id },
+      {
+        where: {
+          userId: guestUser.id
+        }
+      }
+    );
+    console.log('UPDATED ORDERS');
+  } catch (e) {
+    console.log('FAILED TO UPDATE ORDERS FOR NEW USER');
+    console.error(e);
+    return new Error(e);
+  }
+  try {
+    await Cart.update(
+      { userId: newUser.id },
+      {
+        where: {
+          userId: guestUser.id
+        }
+      }
+    );
+    console.log('UPDATED CART');
+  } catch (e) {
+    console.log('FAILED TO UPDATE CART FOR NEW USER');
+    console.error(e);
+    return new Error(e);
+  }
+  return newUser;
+};
+
+module.exports = {
+  paginate,
+  UserObject,
+  CartObject,
+  OrderObject,
+  mergeAndDestroyUser
+};
